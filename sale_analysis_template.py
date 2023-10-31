180a48765eb717a8db1a2a27d1dcd2c70e42ddad
William J Burbridge <123124890+WillBridge0789@users.noreply.github.com>
2023-10-31 17:30:51 -0400

    Add files via upload

---
+++ sale_analysis_template.py
@@ -0,0 +1,29 @@
+"""
+    Please write a script to read in the sale results csv file, remove all rows where out is true ('t')
+    Using pandas plot method to create the following 4 png files
+    - Total sales for the top 10 sires by total sale price
+    - Avg sale price for the top 10 buyers by total sale price
+    - Avg sale price by book and by sex
+    - Avg sale price by month of birth
+"""
+# Need to install pandas library 
+import pandas as pd
+
+# Need to install plotly library 
+pd.options.plotting.backend = "plotly"
+
+# File path
+file_path = #Path to the supplied CSV file "keesep_2023_results.csv"
+
+# Your code here
+# 1. Read in the csv file to pandas dataframe
+
+# 2. Remove outs from the data
+
+# 3. Total sale for the top 10 sires by gross sale price
+
+# 4. Avg sale price for top 10 buyers by gross sale price
+
+# 5. Avg sale price by book by sex
+
+# 6. Avg sale price by month of birth
